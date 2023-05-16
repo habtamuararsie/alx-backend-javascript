f892bdaa8f23e5a7a6df5b42fbf6036ce10a2699
unknown <habtamuararsie@yahoo.com>
2023-05-16 11:59:50 +0300

    node js basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * A function which dispaly a message to the STDOUT
+ * @param {String} message the message to be display
+ * @author Habtamu Ararsie <https://github.com/habtamuararsie>
+ */
+const displayMessage = (message) => {
+    console.log(message);
+  };
+  
+  module.exports = displayMessage;
