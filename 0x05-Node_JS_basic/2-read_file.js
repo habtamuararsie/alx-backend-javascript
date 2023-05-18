6f16e039204760e8a34f1f0060899f3a877a7ba6
unknown <habtamuararsie@yahoo.com>
2023-05-18 15:49:45 +0300

    node js basic

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 
 /**
  * Counts the students in a CSV data file.
@@ -17,32 +17,32 @@ const countStudents = (dataPath) => {
     .toString("utf-8")
     .trim()
     .split("\n");
-  const studGroups = {};
+  const studentGroups = {};
   const dbFieldNames = fileLines[0].split(",");
   const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
   for (const line of fileLines.slice(1)) {
-    const studRecord = line.split(",");
-    const studentPropValues = studRecord.slice(0, studRecord.length - 1);
-    const field = studRecord[studRecord.length - 1];
-    if (!Object.keys(studGroups).includes(field)) {
-      studGroups[field] = [];
+    const studentRecord = line.split(",");
+    const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+    const field = studentRecord[studentRecord.length - 1];
+    if (!Object.keys(studentGroups).includes(field)) {
+      studentGroups[field] = [];
     }
-    const studEntries = studentPropNames.map((propName, idx) => [
+    const studentEntries = studentPropNames.map((propName, idx) => [
       propName,
       studentPropValues[idx],
     ]);
-    studGroups[field].push(Object.fromEntries(studEntries));
+    studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
-  const totalStud = Object.values(studGroups).reduce(
+  const totalStudents = Object.values(studentGroups).reduce(
     (pre, cur) => (pre || []).length + cur.length
   );
-  console.log(`Number of students: ${totalStud}`);
-  for (const [field, group] of Object.entries(studGroups)) {
-    const studNames = group.map((stud) => student.firstname).join(", ");
+  console.log(`Number of students: ${totalStudents}`);
+  for (const [field, group] of Object.entries(studentGroups)) {
+    const studentNames = group.map((student) => student.firstname).join(", ");
     console.log(
-      `Number of students in ${field}: ${group.length}. List: ${studNames}`
+      `Number of students in ${field}: ${group.length}. List: ${studentNames}`
     );
   }
 };
